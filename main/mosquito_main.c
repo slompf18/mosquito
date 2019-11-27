c592b706f12ee277f86257e85d8b7564c4333d7f
slompf <torsten.goroll@gmx.de>
2019-11-27 10:22:10 +0100

    Improvements and cleanup.

--- main/mosquito_main.c
+++ main/mosquito_main.c
@@ -8,10 +8,14 @@
 
 #include <esp_system.h>
 #include <esp_log.h>
-#include <FreeRTOS.h>
-#include <semphr.h>
-#include <task.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <freertos/task.h>
 #include <driver/gpio.h>
+#include <nvs_flash.h>
+#include <esp_netif.h>
+#include <esp_event.h>
+#include <esp_sntp.h>
 
 #define IO_PIR 21
 SemaphoreHandle_t guard = NULL;
@@ -50,15 +54,13 @@ void on_wifi_initialized()
 {
     if(!initialized)
     {
-        ESP_LOGI(LOGNAME, "Wifi initalized!");
-        hk_init("Mosquito", HK_CAT_SWITCH, "111-22-222", ESP_LOG_DEBUG);
-        //hk_reset();
-        hk_setup_start();
-        app_characteristic = hk_setup_add_motion_sensor(
-            "Mosquito", "Slompf Industries", "A motion sensor.", "0000001", "0.1",
-            true, on_read);
-        hk_setup_finish();
+        sntp_setoperatingmode(SNTP_OPMODE_POLL);
+        sntp_setservername(0, "pool.ntp.org");
+        sntp_init();
 
+        ESP_LOGI(LOGNAME, "Wifi initalized!");
+        hk_init();
+        
         //init worker task
         vSemaphoreCreateBinary(guard);
         xTaskCreate(motion_handler, "motion_handler", 3072, NULL, 10, NULL);
@@ -76,6 +78,22 @@ void app_main()
 {
     ESP_LOGI(LOGNAME, "SDK version:%s\n", esp_get_idf_version());
     ESP_LOGI(LOGNAME, "Starting");
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    esp_netif_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    hk_setup_start("Mosquito", HK_CAT_SENSOR, "111-22-222");
+    app_characteristic = hk_setup_add_motion_sensor(
+        "Mosquito", "Slompf Industries", "A motion sensor.", "0000001", "0.1",
+        true, on_read);
+    hk_setup_finish();
+    //hk_reset();
 
     // init wlan
     ESP_LOGD(LOGNAME, "Initializing wifi!\n");
--- main/simpleWifi.c
+++ main/simpleWifi.c
@@ -4,64 +4,45 @@
 #include <esp_wifi.h>
 #include <esp_log.h>
 #include <esp_err.h>
-#include <esp_event_loop.h>
-#include <nvs_flash.h>
+#include <esp_event.h>
 
 #define LOG_NAME "Simple wifi"
 
-static esp_err_t event_handler(void *ctx, system_event_t *event)
+static void event_handler(void* args, esp_event_base_t event_base, 
+                                int32_t event_id, void* event_data)
 {
-    //todo: check on router off/on
-    switch (event->event_id)
-    {
-    case SYSTEM_EVENT_STA_START:
-        ESP_LOGD(LOG_NAME, "Wifi driver started, connecting to wifi.");
+    ESP_LOGI(LOG_NAME, "Got event: %s, %d", event_base, event_id);
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        ESP_LOGI(LOG_NAME, "Wifi driver started, connecting to wifi.");
         esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        ESP_LOGD(LOG_NAME, "Wifi driver got ip: %s. Calling handler.",
-                 ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-
-        SimpleWifiCallback cb = (SimpleWifiCallback)ctx;
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGI(LOG_NAME, "Wifi driver was disconnected. Reconnecting...");
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ESP_LOGI(LOG_NAME, "Got event: got ip");
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(LOG_NAME, "Wifi driver got ipCalling handler: "IPSTR, IP2STR(&event->ip_info.ip));
+        SimpleWifiCallback cb = (SimpleWifiCallback)args;
         cb();
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-        ESP_LOGD(LOG_NAME, "Wifi driver was disconnected. Reconnecting...");
-        esp_wifi_connect();
-        break;
-
-    case SYSTEM_EVENT_AP_STACONNECTED:
-        ESP_LOGD(LOG_NAME, "A station connected to our soft access point:" MACSTR " join, AID=%d",
-                 MAC2STR(event->event_info.sta_connected.mac),
-                 event->event_info.sta_connected.aid);
-        break;
-    case SYSTEM_EVENT_AP_STADISCONNECTED:
-        ESP_LOGD(LOG_NAME, "A station disconnected from our soft access point:" MACSTR "leave, AID=%d",
-                 MAC2STR(event->event_info.sta_disconnected.mac),
-                 event->event_info.sta_disconnected.aid);
-        break;
-    default:
-        break;
     }
-
-    return ESP_OK;
 }
 
 void init_simple_wifi(char *ssid, char *passwd, void *callback)
 {
-    ESP_ERROR_CHECK(nvs_flash_init());
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, callback));
+    esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, callback));
+
     wifi_config_t wifi_config = {};
 
     memcpy(&wifi_config.sta.ssid, ssid, strlen(ssid));
     memcpy(&wifi_config.sta.password, passwd, strlen(passwd));
 
-    ESP_LOGD(LOG_NAME, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_LOGI(LOG_NAME, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
