7044975e2d507ee65b1863846a725e0c1e18db06
slompf <torsten.goroll@gmx.de>
2020-02-28 16:26:20 +0100

    cleanup

--- main/mosquito_main.c
+++ main/mosquito_main.c
@@ -1,8 +1,5 @@
-#define SSID "Ways"
-#define PASS "ShowMeTheWorld"
 #define LOGNAME "App"
 
-#include "./simpleWifi.h"
 #include <homekit.h>
 #include <homekit_fascade.h>
 
@@ -13,9 +10,7 @@
 #include <freertos/task.h>
 #include <driver/gpio.h>
 #include <nvs_flash.h>
-#include <esp_netif.h>
 #include <esp_event.h>
-#include <esp_sntp.h>
 
 #define IO_PIR 21
 SemaphoreHandle_t guard = NULL;
@@ -50,30 +45,6 @@ void motion_handler(void *args_ptr)
     }
 }
 
-void on_wifi_initialized()
-{
-    if(!initialized)
-    {
-        sntp_setoperatingmode(SNTP_OPMODE_POLL);
-        sntp_setservername(0, "pool.ntp.org");
-        sntp_init();
-
-        ESP_LOGI(LOGNAME, "Wifi initalized!");
-        hk_init();
-        
-        //init worker task
-        vSemaphoreCreateBinary(guard);
-        xTaskCreate(motion_handler, "motion_handler", 3072, NULL, 10, NULL);
-
-        // init io
-        gpio_set_direction(IO_PIR, GPIO_MODE_INPUT);
-        gpio_set_intr_type(IO_PIR, GPIO_INTR_ANYEDGE);
-        gpio_install_isr_service(0);
-        gpio_isr_handler_add(IO_PIR, on_isr, (void *)IO_PIR);
-            initialized = true;
-    }
-}
-
 void app_main()
 {
     ESP_LOGI(LOGNAME, "SDK version:%s\n", esp_get_idf_version());
@@ -85,10 +56,8 @@ void app_main()
       ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
-    esp_netif_init();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    hk_setup_start("Mosquito", HK_CAT_SENSOR, "111-22-222");
+    hk_setup_start();
     app_characteristic = hk_setup_add_motion_sensor(
         "Mosquito", "Slompf Industries", "A motion sensor.", "0000001", "0.1",
         true, on_read);
@@ -96,6 +65,17 @@ void app_main()
     //hk_reset();
 
     // init wlan
-    ESP_LOGD(LOGNAME, "Initializing wifi!\n");
-    init_simple_wifi(SSID, PASS, on_wifi_initialized);
+    ESP_LOGI(LOGNAME, "Wifi initalized!");
+    hk_init("Bla", HK_CAT_OTHER, "111-22-222");
+    
+    //init worker task
+    vSemaphoreCreateBinary(guard);
+    xTaskCreate(motion_handler, "motion_handler", 3072, NULL, 10, NULL);
+
+    // init io
+    gpio_set_direction(IO_PIR, GPIO_MODE_INPUT);
+    gpio_set_intr_type(IO_PIR, GPIO_INTR_ANYEDGE);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(IO_PIR, on_isr, (void *)IO_PIR);
+        initialized = true;
 }
--- main/simpleWifi.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-typedef void (*SimpleWifiCallback)(void);
-
-void init_simple_wifi(char* ssid, char* passwd, void* callback);
