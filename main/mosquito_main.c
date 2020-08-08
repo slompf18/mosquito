521c5d579b7bd3d4cd23d4a4fbcccb1f028f5192
slompf <torsten.goroll@gmx.de>
2020-08-08 15:41:50 +0200

    Updates due to changes in framework

--- main/mosquito_main.c
+++ main/mosquito_main.c
@@ -23,17 +23,18 @@ void on_identify()
     ESP_LOGI(LOGNAME, "Identify");
 }
 
-esp_err_t on_write(hk_mem* request)
+esp_err_t on_write(hk_mem *request)
 {
-    ESP_LOGI(LOGNAME, "Write");
-    app_sth_is_moving = *(bool*)request->ptr;
-    ESP_LOGI(LOGNAME, "Write: %d", app_sth_is_moving);
+    ESP_LOGI(LOGNAME, "Write\n");
+    app_sth_is_moving = *(bool *)request->ptr;
+    ESP_LOGI(LOGNAME, "Write: %d\n", app_sth_is_moving);
     return ESP_OK;
 }
 
-esp_err_t on_read(hk_mem* response)
+esp_err_t on_read(hk_mem *response)
 {
-    ESP_LOGI(LOGNAME, "Read");
+    ESP_LOGI(LOGNAME, "Read: %d", app_sth_is_moving);
+    ESP_LOGD(LOGNAME, "Sizeof bool: %d", sizeof(bool));
     hk_mem_append_buffer(response, &app_sth_is_moving, sizeof(bool));
     return ESP_OK;
 }
@@ -54,8 +55,14 @@ void motion_handler(void *args_ptr)
         }
         else if (xSemaphoreTake(guard, delay) == pdTRUE)
         {
+            bool sth_was_moving = app_sth_is_moving;
             app_sth_is_moving = gpio_get_level(IO_PIR);
-            hk_notify(app_chr);
+
+            if (sth_was_moving != app_sth_is_moving)
+            {
+                ESP_LOGI(LOGNAME, "Something is moving: %d", app_sth_is_moving);
+                hk_notify(app_chr);
+            }
         }
     }
 }
@@ -63,36 +70,32 @@ void motion_handler(void *args_ptr)
 void app_main()
 {
     ESP_LOGI(LOGNAME, "SDK version:%s\n", esp_get_idf_version());
-    ESP_LOGI(LOGNAME, "Starting");
+    ESP_LOGI(LOGNAME, "Starting\n");
 
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    // app_chr = hk_setup_add_switch("Mosquito", "Slompf Industries", "A switch.", "0000001", "0.2",
+    //                               on_identify, on_read, on_write);
+    app_chr = hk_setup_add_motion_sensor(
+        "Mosquito", "Slompf Industries", "A motion sensor.", "0000001", "0.1",
+        on_identify, on_read);
 
-    hk_setup_start();
-    app_chr = hk_setup_add_switch("Mosquito", "Slompf Industries", "A switch.", "0000001", "0.2",
-        true, on_identify, on_read, on_write);
-    // app_chr = hk_setup_add_motion_sensor(
-    //     "Mosquito", "Slompf Industries", "A motion sensor.", "0000001", "0.1",
-    //     true, on_read);
-    hk_setup_finish();
-    hk_reset();
+    //hk_reset();
 
     // init wlan
-    ESP_LOGI(LOGNAME, "Wifi initalized!");
     hk_init("Blu", HK_CAT_OTHER, "111-22-222");
-    
+
     //init worker task
     vSemaphoreCreateBinary(guard);
     xTaskCreate(motion_handler, "motion_handler", 3072, NULL, 10, NULL);
 
-    // init io
-    gpio_set_direction(IO_PIR, GPIO_MODE_INPUT);
-    gpio_set_intr_type(IO_PIR, GPIO_INTR_ANYEDGE);
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(IO_PIR, on_isr, (void *)IO_PIR);
-        initialized = true;
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.pin_bit_mask = 1ULL << IO_PIR;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 0;
+    io_conf.pull_down_en = 1;
+    gpio_config(&io_conf);
+
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
+    ESP_ERROR_CHECK(gpio_isr_handler_add(IO_PIR, on_isr, (void *)IO_PIR));
+    initialized = true;
 }
