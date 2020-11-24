158806f878f5bc0b37b0349a51530034a6ad34b7
slompf <torsten.goroll@gmx.de>
2020-11-24 10:26:16 +0100

    Adaptions to new interface.

--- main/mosquito_main.c
+++ main/mosquito_main.c
@@ -74,9 +74,9 @@ void app_main()
 
     // app_chr = hk_setup_add_switch("Mosquito", "Slompf Industries", "A switch.", "0000001", "0.2",
     //                               on_identify, on_read, on_write);
-    app_chr = hk_setup_add_motion_sensor(
-        "Mosquito", "Slompf Industries", "A motion sensor.", "0000001", "0.2.16",
-        on_identify, on_read);
+    hk_setup_add_motion_sensor(
+        "Mosquito", "Slompf Industries", "A motion sensor.", "0000001", "0.2.17",
+        on_identify, on_read, &app_chr);
 
     // hk_reset();
 
